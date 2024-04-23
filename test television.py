8e78a0c502787ba8bc6e372fba9451a2af202e87
WorkingSiege <th3boss057@gmail.com>
2024-04-23 00:21:14 -0500

    Adjusted unittest to pytest

--- test television.py
+++ test television.py
@@ -1,76 +1,73 @@
-import unittest
+import pytest
 from television import Television
 
-class TeleTest(unittest.TestCase):
-    def setUp(self):
-        self.tv = Television()
+def tv():
+    return Television()
 
-    def test_init(self):
-        self.assertFalse(self.tv.status)
-        self.assertEqual(self.tv.volume, Television.MIN_VOLUME)
-        self.assertEqual(self.tv.channel, Television.MIN_CHANNEL)
+def test_init(tv):
+    assert not tv._Television__status
+    assert tv._Television__volume == Television.MIN_VOLUME
+    assert tv._Television__channel == Television.MIN_CHANNEL
 
-    def testPower(self):
-        self.tv.power()
-        self.assertTrue(self.tv.status)
-        self.tv.power()
-        self.assertFalse(self.tv.status)
+def test_power(tv):
+    tv.power()
+    assert tv._Television__status
+    tv.power()
+    assert not tv._Television__status
 
-    def testMute(self):
-        self.tv.power()
-        self.tv.volume_up()
-        self.tv.mute()
-        self.assertEqual(self.tv.muted, True)
-        self.tv.mute()
-        self.assertEqual(self.tv.muted, False)
+def test_mute(tv):
+    tv.power()
+    tv.volume_up()
+    tv.mute()
+    assert tv._Television__muted
+    tv.mute()
+    assert not tv._Television__muted
 
-    def testChUp(self):
-        self.tv.channel_up()
-        self.assertEqual(self.tv.channel, Television.MIN_CHANNEL)
-        self.tv.power()
-        self.tv.channel_up()
-        self.assertEqual(self.tv.channel, Television.MIN_CHANNEL + 1)
-        for i in range(Television.MAX_CHANNEL - Television.MIN_CHANNEL):
-            self.tv.channel_up()
-        self.assertEqual(self.tv.channel, Television.MIN_CHANNEL)
+def test_channel_up(tv):
+    tv.channel_up()
+    assert tv._Television__channel == Television.MIN_CHANNEL
+    tv.power()
+    tv.channel_up()
+    assert tv._Television__channel == Television.MIN_CHANNEL + 1
+    for _ in range(Television.MAX_CHANNEL - Television.MIN_CHANNEL):
+        tv.channel_up()
+    assert tv._Television__channel == Television.MIN_CHANNEL
 
-    def testChDw(self):
-        self.tv.channel_down()
-        self.assertEqual(self.tv.channel, Television.MIN_CHANNEL)
-        self.tv.power()
-        self.tv.channel_down()
-        self.assertEqual(self.tv.channel, Television.MIN_CHANNEL - 1)
-        for _ in range(Television.MAX_CHANNEL - Television.MIN_CHANNEL + 1):
-            self.tv.channel_down()
-        self.assertEqual(self.tv.channel, Television.MAX_CHANNEL)
+def test_channel_down(tv):
+    tv.channel_down()
+    assert tv._Television__channel == Television.MIN_CHANNEL
+    tv.power()
+    tv.channel_down()
+    assert tv._Television__channel == Television.MIN_CHANNEL - 1
+    for _ in range(Television.MAX_CHANNEL - Television.MIN_CHANNEL + 1):
+        tv.channel_down()
+    assert tv._Television__channel == Television.MAX_CHANNEL
+def test_volume_up(tv):
+    tv.volume_up()
+    assert tv._Television__volume == Television.MIN_VOLUME
+    tv.power()
+    tv.volume_up()
+    assert tv._Television__volume == Television.MIN_VOLUME + 1
+    tv.mute()
+    tv.volume_up()
+    assert tv._Television__volume == Television.MIN_VOLUME + 2
+    for _ in range(Television.MAX_VOLUME - Television.MIN_VOLUME + 1):
+        tv.volume_up()
+    assert tv._Television__volume == Television.MAX_VOLUME
 
-    def testVolUp(self):
-        self.tv.volume_up()
-        self.assertEqual(self.tv.volume, Television.MIN_VOLUME)
-        self.tv.power()
-        self.tv.volume_up()
-        self.assertEqual(self.tv.volume, Television.MIN_VOLUME + 1)
-        self.tv.mute()
-        self.tv.volume_up()
-        self.assertEqual(self.tv.volume, Television.MIN_VOLUME + 2)
-        for i in range(Television.MAX_VOLUME - Television.MIN_VOLUME + 1):
-            self.tv.volume_up()
-        self.assertEqual(self.tv.volume, Television.MAX_VOLUME)
-
-    def testVolDw(self):
-        self.tv.volume_down()
-        self.assertEqual(self.tv.volume, Television.MIN_VOLUME)
-        self.tv.power()
-        self.tv.volume_up()
-        self.tv.volume_down()
-        self.assertEqual(self.tv.volume, Television.MAX_VOLUME - 1)
-        self.tv.mute()
-        self.tv.volume_down()
-        self.assertEqual(self.tv.volume, Television.MIN_VOLUME)
-        for _ in range(Television.MAX_VOLUME - Television.MIN_VOLUME + 1):
-            self.tv.volume_down()
-        self.assertEqual(self.tv.volume, Television.MIN_VOLUME)
+def test_volume_down(tv):
+    tv.volume_down()
+    assert tv._Television__volume == Television.MIN_VOLUME
+    tv.power()
+    tv.volume_up()
+    tv.volume_down()
+    assert tv._Television__volume == Television.MAX_VOLUME - 1
+    tv.mute()
+    tv.volume_down()
+    assert tv._Television__volume == Television.MIN_VOLUME
+    for _ in range(Television.MAX_VOLUME - Television.MIN_VOLUME + 1):
+        tv.volume_down()
+    assert tv._Television__volume == Television.MIN_VOLUME
 
 if __name__ == "__main__":
-    unittest.main()
-
+    pytest.main()
